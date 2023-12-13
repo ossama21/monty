a5394b0940376e2ec95abf8ea0a357fbe0f7c5d6
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2023-12-13 12:41:43 -0800

    all

---
+++ stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * f_stack - multiplies the top two elements of the stack.
+ * @stack: stack head
+ * @line_number: line_number
+ * Return: nothing
+ */
+
+void f_stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	bus.is_stack = 0;
+}
+
