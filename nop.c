acaadb7bce58c8c107cb85657418744832353359
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2023-12-13 12:51:02 -0800

    all

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - doesn’t do anything.
+ * @stack: stack header
+ * @line_number: line number
+ * Return: 0
+*/
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
