42e57a9715981711a8e3c98b2f125956c20d4221
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2023-12-13 12:32:44 -0800

    all

---
+++ mod.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * mod - mods the top two elements of the stack.
+ * @stack: stack head
+ * @line_number: line_number
+ * Return: nothing
+ */
+
+void mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = *stack;
+	int len = 0, sum = 0;
+
+
+	while (head)
+	{
+		head = head->next;
+		len++;
+	}
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		free_stack(*stack);
+		cleanup_and_exit();
+	}
+	head = *stack;
+
+	if (head->next->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_stack(*stack);
+		cleanup_and_exit();
+	}
+
+	sum = head->next->n % head->n;
+	head->next->n = sum;
+	*stack = head->next;
+	free(head);
+}
+
